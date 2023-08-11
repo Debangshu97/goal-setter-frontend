43fc1869f9d1d4b57f2b39f505fd8ecdc8948635
Debangshu <kanthadebangshu97@gmail.com>
2023-08-11 23:54:53 +0530

    frontend register login logout complete

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,6 +2,9 @@ import React, { useState,useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { FaUser } from 'react-icons/fa'
+import { register, reset } from '../features/auth/authSlice'
+import { toast } from 'react-toastify'
+import Spinner from '../components/Spinner'
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -13,9 +16,45 @@ const Register = () => {
 
    const { name, email, password, password2 } = formData
     const navigate = useNavigate()
+    const dispatch=useDispatch()
+
+      const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
     const onSubmit=(e)=>{
         e.preventDefault();
+        
+        if (password !== password2) {
+      toast.error('Passwords do not match')
+    } 
+    else {
+      const userData = {
+        name,
+        email,
+        password,
+      }
+
+      dispatch(register(userData))
     }
+    }
+
+    if (isLoading) {
+    return <Spinner />
+  }
+
     const onChange=(e)=>{
         setFormData((prevState) => ({
       ...prevState,
