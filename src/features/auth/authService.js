bfbfb74c6e13b45aade1b13a4ffc3fd7b8c9802b
Debangshu <kanthadebangshu97@gmail.com>
2023-08-17 23:25:21 +0530

    modifications

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = '/api/users/'
+const API_URL = 'https://goalsetterbackend-t94b.onrender.com/api/users/'
 
 // Register user
 const register = async (userData) => {
